a9f0e114ba647ecdc8a52ce74a908f222c7d686a
Jan Boček <honza.bocek@gmail.com>
2018-10-16 15:25:38 +0200

    xxx

--- article.md
+++ article.md
@@ -3,8 +3,8 @@ perex: "XXX"
 published: "21. dubna 2018"
 coverimg: https://interaktivni.rozhlas.cz/brexit/media/cover.jpg
 coverimg_note: "Foto <a href='#'>ČTK</a>"
-styles: ["//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"]
-libraries: [jquery, "https://code.jquery.com/ui/1.12.1/jquery-ui.js"]
+styles: []
+libraries: [jquery]
 options: [] #wide, noheader (, nopic)
 ---
 
@@ -12,17 +12,304 @@ Je to prosté, stačí do <span style="background-color: #2171b5;color: white;di
 
 <wide>
 <div id="container">
-	<div id="obec">
-		<h3>Senátní obvod</h3>
-		<form onsubmit="return false">
-			<div class="autocomplete" style="width:300px;">
-				<input id="vyberObce" name="vyberObce" type="text" placeholder="Napište jméno obvodu">
-			</div>
-		</form>
-	</div>
-	<div id="kandidati"><table id="tabulkaKandidatu" class="display" style="width:100%"></table></div>
-	<div id="zpet"></div>
+<div id="alternativni">
+
+<h3>Alternativní hlasování</h3>
+<table>
+<thead><tr>
+<th>Kandidát</th>
+<th align="right">Věk</th>
+<th>Strana</th>
+<th>Povolání</th>
+<th>Bydliště</th>
+<th>Preference</th>
+</tr>
+</thead>
+<tbody><tr>
+<td>Gabal Ivan PhDr.</td>
+<td align="right">67</td>
+<td>Zelení</td>
+<td>sociolog</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="1"></td>
+</tr>
+<tr>
+<td>Hilšer Marek MUDr. Bc. Ph.D.</td>
+<td align="right">42</td>
+<td>MHS</td>
+<td>lékař, vysokoškolský pedagog</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="2"></td>
+</tr>
+<tr>
+<td>Hujová Vladislava Ing.</td>
+<td align="right">55</td>
+<td>HPP</td>
+<td>ekonomka, starostka MČ P-3 v období 2010-2018</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="3"></td>
+</tr>
+<tr>
+<td>Jakl Ladislav</td>
+<td align="right">58</td>
+<td>SPD</td>
+<td>publicista</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="4"></td>
+</tr>
+<tr>
+<td>Kerekeš Roman MUDr.</td>
+<td align="right">49</td>
+<td>ANO</td>
+<td>lékař - kardiolog</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="5"></td>
+</tr>
+<tr>
+<td>Kratina Vladimír</td>
+<td align="right">66</td>
+<td>ODS</td>
+<td>herec</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="6"></td>
+</tr>
+<tr>
+<td>Michálek Libor Mgr. MPA</td>
+<td align="right">49</td>
+<td>Piráti</td>
+<td>senátor a ekonom</td>
+<td>Chýně</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="7"></td>
+</tr>
+<tr>
+<td>Roškot Vladimír Ing.</td>
+<td align="right">68</td>
+<td>KSČM</td>
+<td>podnikatel v oblasti obchodu</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="8"></td>
+</tr>
+<tr>
+<td>Srb Václav</td>
+<td align="right">60</td>
+<td>KČ</td>
+<td>průvodce, vinárník, tlumočník</td>
+<td>Praha</td>
+<td><input type="number" min="1" max="9" step="1" placeholder="1-9" class="alternativni" id="9"></td>
+</tr>
+</tbody></table>
+
+</div>
+
+<div id="hlasovat">
+<button type="button" id="hlasovaniAlternativni" onClick="hlasovani('alternativni')">Hlasovat</button>
+</div>
+
 </div>
 </wide>
 
-_Zdroj dat: [volby.cz](https://volby.cz/pls/kv2018/kv?xjazyk=CZ&xid=1)_
+<wide>
+<div id="container">
+<div id="approval">
+
+<h3>Schvalující hlasování</h3>
+<table>
+<thead><tr>
+<th>Kandidát</th>
+<th align="right">Věk</th>
+<th>Strana</th>
+<th>Povolání</th>
+<th>Bydliště</th>
+<th>Schválení</th>
+</tr>
+</thead>
+<tbody><tr>
+<td>Gabal Ivan PhDr.</td>
+<td align="right">67</td>
+<td>Zelení</td>
+<td>sociolog</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="1"></td>
+</tr>
+<tr>
+<td>Hilšer Marek MUDr. Bc. Ph.D.</td>
+<td align="right">42</td>
+<td>MHS</td>
+<td>lékař, vysokoškolský pedagog</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="2"></td>
+</tr>
+<tr>
+<td>Hujová Vladislava Ing.</td>
+<td align="right">55</td>
+<td>HPP</td>
+<td>ekonomka, starostka MČ P-3 v období 2010-2018</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="3"></td>
+</tr>
+<tr>
+<td>Jakl Ladislav</td>
+<td align="right">58</td>
+<td>SPD</td>
+<td>publicista</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="4"></td>
+</tr>
+<tr>
+<td>Kerekeš Roman MUDr.</td>
+<td align="right">49</td>
+<td>ANO</td>
+<td>lékař - kardiolog</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="5"></td>
+</tr>
+<tr>
+<td>Kratina Vladimír</td>
+<td align="right">66</td>
+<td>ODS</td>
+<td>herec</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="6"></td>
+</tr>
+<tr>
+<td>Michálek Libor Mgr. MPA</td>
+<td align="right">49</td>
+<td>Piráti</td>
+<td>senátor a ekonom</td>
+<td>Chýně</td>
+<td><input type="checkbox" class="approval" id="7"></td>
+</tr>
+<tr>
+<td>Roškot Vladimír Ing.</td>
+<td align="right">68</td>
+<td>KSČM</td>
+<td>podnikatel v oblasti obchodu</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="8"></td>
+</tr>
+<tr>
+<td>Srb Václav</td>
+<td align="right">60</td>
+<td>KČ</td>
+<td>průvodce, vinárník, tlumočník</td>
+<td>Praha</td>
+<td><input type="checkbox" class="approval" id="9"></td>
+</tr>
+</tbody></table>
+
+</div>
+
+<div id="hlasovat">
+<button type="button" id="hlasovaniApproval" onClick="hlasovani('approval')">Hlasovat</button>
+</div>
+
+</div>
+</wide>
+
+<wide>
+<div id="container">
+<div id="janecek">
+
+<h3>Janečkův systém</h3>
+<table>
+<thead><tr>
+<th>Kandidát</th>
+<th align="right">Věk</th>
+<th>Strana</th>
+<th>Povolání</th>
+<th>Bydliště</th>
+<th>+</th>
+<th>-</th>
+</tr>
+</thead>
+<tbody><tr>
+<td>Gabal Ivan PhDr.</td>
+<td align="right">67</td>
+<td>Zelení</td>
+<td>sociolog</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="1" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="1" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Hilšer Marek MUDr. Bc. Ph.D.</td>
+<td align="right">42</td>
+<td>MHS</td>
+<td>lékař, vysokoškolský pedagog</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="2" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="2" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Hujová Vladislava Ing.</td>
+<td align="right">55</td>
+<td>HPP</td>
+<td>ekonomka, starostka MČ P-3 v období 2010-2018</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="3" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="3" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Jakl Ladislav</td>
+<td align="right">58</td>
+<td>SPD</td>
+<td>publicista</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="4" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="4" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Kerekeš Roman MUDr.</td>
+<td align="right">49</td>
+<td>ANO</td>
+<td>lékař - kardiolog</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="5" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="5" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Kratina Vladimír</td>
+<td align="right">66</td>
+<td>ODS</td>
+<td>herec</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="6" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="6" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Michálek Libor Mgr. MPA</td>
+<td align="right">49</td>
+<td>Piráti</td>
+<td>senátor a ekonom</td>
+<td>Chýně</td>
+<td><input type="checkbox" class="janecekplus" id="7" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="7" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Roškot Vladimír Ing.</td>
+<td align="right">68</td>
+<td>KSČM</td>
+<td>podnikatel v oblasti obchodu</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="8" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="8" onClick="janecekCall()" disabled="true"></td>
+</tr>
+<tr>
+<td>Srb Václav</td>
+<td align="right">60</td>
+<td>KČ</td>
+<td>průvodce, vinárník, tlumočník</td>
+<td>Praha</td>
+<td><input type="checkbox" class="janecekplus" id="9" onClick="janecekCall()"></td>
+<td><input type="checkbox" class="janecekminus" id="9" onClick="janecekCall()" disabled="true"></td>
+</tr>
+</tbody></table>
+
+</div>
+
+<div id="hlasovat">
+<button type="button" id="hlasovaniJanecek" onClick="hlasovani('janecek')">Hlasovat</button>
+</div>
+
+</div>
+</wide>
--- js/script.js
+++ js/script.js
@@ -1,441 +1,72 @@
-// JS soubory ve složce /styles/ se do výsledného článku zakompilují automaticky
+function janecekCall() {
 
-$.ajax({
-  type: "GET",
-  url: "https://data.irozhlas.cz/volby-obecni-2018/data/kandidatky/app/obce/nazvyobci.csv",
-  dataType: "text",
-  success: function(data) {hledatko($.csv.toObjects(data));}
-});
+  var plusJanecci = 0,
+      minusJanecci = 0;
 
-function hledatko(data) {
-  var nazvyObci = data.sort(function(a, b) {
-    if(a.NAZEVFULL < b.NAZEVFULL) return -1;
-    if(a.NAZEVFULL > b.NAZEVFULL) return 1;
-    return 0;
-  });
-
-	var seznamObci = nazvyObci.map(function(d) {
-		return d['NAZEVFULL'];
-	});
-
-  var obceHledatko = []
-  seznamObci.forEach(function(obec){obceHledatko.push([obec, obec.split(" (")[0]]);});
-
-  $('#vyberObce').autocomplete({
-    delay: 500,
-    source: function(request, response){
-      var result = obceHledatko.filter(function(obec) {
-        return obec[1].toLowerCase().includes(request.term.toLowerCase());
-      });
-      response(result.map(function(obec){return obec[0];}));
-    },
-    select: function(e) {
-      document.getElementById("strany").innerHTML = 'Načítám data...'
-      setTimeout(function() {
-        document.getElementById("strany").innerHTML = 'Načítám data...'
-        var zvolenaObec = document.getElementById("vyberObce").value;
-        var index = seznamObci.indexOf(zvolenaObec);
-        var idObce = nazvyObci[index]['KODZASTUP'];
-        ukazStrany(zvolenaObec, idObce);
-      }, 250);
+  for(var i = 0; i < 9; i++) {
+    if(document.getElementsByClassName("janecekplus")[i].checked) {
+      plusJanecci++;
     }
-  });
-};
-
-function ukazStrany(zvolenaObec, idObce) {
-
-  $.ajax({
-    type: "GET",
-    url: "https://data.irozhlas.cz/volby-obecni-2018/data/kandidatky/tip/" + idObce + ".csv",
-    dataType: "text",
-    success: function(data) {pokracuj($.csv.toObjects(data));}
-  });
-
-	function pokracuj(strany) {
-    var idStran = strany.map(function(d) {
-      return d['StranaNr'];
-    });
-
-    var stranyBezId = strany.map(function(d) {
-      delete(d['StranaNr']);
-      return (d);
-    });
-
-    for(i = 0; i < stranyBezId.length; i++) {
-      stranyBezId[i]['Hlasy (%)'] = 0
-      stranyBezId[i]['Mandáty'] = 0;
+    if(document.getElementsByClassName("janecekminus")[i].checked) {
+      minusJanecci++;
     }
+  }
 
-    var html = '<h3>Kandidující strany</h3>'
-    html += '<table id="tabulkaStran" class="display" style="width:100%"></table>'
-    html += '<div id="zpetStrany"><button type="button" id="unter">Pro rozdělení mandátů je potřeba rozdat ještě 100 % hlasů</button></div>'
-
-    document.getElementById("strany").innerHTML = html;
-
-    poskladejTabulkuStran(stranyBezId, idStran, idObce);
-
-    document.getElementById("obec").style["padding-bottom"] = "30px";
-
-    $(function() {
-      $('#tabulkaStran').DataTable({
-          columnDefs: [
-            { targets: 0, type: 'diacritics-neutralise' },
-            { targets: [3, 4], orderable: false}
-          ],
-          "order": [[ 0, "asc" ]],
-          responsive: {
-              details: {
-                  display: $.fn.dataTable.Responsive.display.childRowImmediate,
-                  type: ''
-              }
-          },
-          "ordering": true,
-          "paging": false,
-          "bInfo": false,
-          "language": {
-              "url": "https://interaktivni.rozhlas.cz/tools/datatables/Czech.json"
-          }
-      });
-    });
-	};
-};
-
-function prepocitejProcenta() {
-  var rozdelenoHlasu = 0;
-  var nejvyssi = 0;
-  var curID = 0;
-  var nejStrana;
-
-  $(".vysledek").each(function() {
-    var vysledek = parseInt(this.value);
-    curID++;
-    if (isNaN(vysledek)) {
-      vysledek = 0;
+  if((plusJanecci < 2) && (minusJanecci < 1)) {
+    for(var i = 0; i < 9; i++) {
+      if (!document.getElementsByClassName("janecekminus")[i].checked) {
+        document.getElementsByClassName("janecekminus")[i].disabled = true;
+      }
+      document.getElementsByClassName("janecekplus")[i].disabled = false;
     }
-    if (vysledek > nejvyssi) {
-      nejvyssi = vysledek;
-      if ($(window).width() < 560) {
-        nejStrana = $(this).parent().parent().parent().parent().parent().parent().parent().prev().find(".nazevStrany")[0].textContent;
+  }
+
+  if((plusJanecci == 2) && (minusJanecci < 1)) {
+    for(var i = 0; i < 9; i++) {
+      if (!document.getElementsByClassName("janecekplus")[i].checked) {
+        document.getElementsByClassName("janecekplus")[i].disabled = true;
       } else {
-        nejStrana = $(this).parent().parent().parent().parent().children()[0].textContent;
+        document.getElementsByClassName("janecekplus")[i].disabled = false;
       }
+      document.getElementsByClassName("janecekminus")[i].disabled = false;
     }
-    rozdelenoHlasu = rozdelenoHlasu + vysledek;
-  })
-
-  var obec = $("#vyberObce").val().split(" (")[0];
-  var vitez = nejStrana.substring(0,1) + nejStrana.substring(1).toLowerCase();
-
-
-  if (rozdelenoHlasu == 100) {
-    var html = '<button type="button" class="klikaci" onclick ="spocitejMandaty()">Spočítat mandáty</button></div>'
-    html += '<div id="sdildiv"></div>';
-  } else if (rozdelenoHlasu > 100) {
-    var html = '<button type="button" id="uber">Pro rozdělení mandátů je potřeba rozdat o ' + (rozdelenoHlasu - 100) + ' % hlasů méně</button></div>'
-  } else {
-    var html = '<button type="button" id="unter">Pro rozdělení mandátů je potřeba rozdat ještě ' + (100 - rozdelenoHlasu) + ' % hlasů</button></div>'
   }
 
-  document.getElementById("zpetStrany").innerHTML = html;
-
-  if (rozdelenoHlasu == 100) {
-    sdilitko(obec, vitez);
+  if((plusJanecci == 2) && (minusJanecci == 1)) {
+    for(var i = 0; i < 9; i++) {
+      if (!document.getElementsByClassName("janecekminus")[i].checked) {
+        document.getElementsByClassName("janecekminus")[i].disabled = true;
+      }
+      document.getElementsByClassName("janecekplus")[i].disabled = true;
+    }
   }
 }
 
-function spocitejMandaty() {
-  var pocetStran = $(".nazevStrany").length
-  var strany = [];
-  var pocetKand = [];
-  var pocetMand;
-  var procenta = [];
-  var falesnyPrah = [];
-  var prah = 5;
-  var prepocet = new Array(pocetStran);
-  var mandaty = [];
-  var mobil = false;
+function hlasovani(system) {
 
-  // vstupy pro výpočet
-  $(".nazevStrany").each(function(){strany.push($(this).text())})
-  $(".pocetKand").each(function(){pocetKand.push($(this).text())});
+  var vysledky = [];
 
-  pocetMand = parseInt(pocetKand[0].substr(pocetKand[0].indexOf('/') + 1, pocetKand[0].length + 1));
-  $(".vysledek").each(function(){procenta.push($(this).val())});
+  vysledky[0] = system;
 
-  // mobilní hack pt. 1
-  if (procenta.length === pocetStran*2) {
-    mobil = true;
-    procenta = procenta.filter(function(procento, index) {return index % 2});
-  }
+  var text = "Děkujeme za hlasování"
 
-  for(i = 0; i < pocetStran; i++) {
-    mandaty.push(0);
-    pocetKand[i] = parseInt(pocetKand[i].substr(0, pocetKand[i].indexOf('/')));
-  }
-
-  // výpočet falešného prahu (bod 3); u obcí, kde může být o třetinu víc kandidátů než mandátů, se nadbyteční kandidáti nepočítají
-  for(i = 0; i < pocetStran; i++) {
-    var pocetKandProFalesnyPrah = parseInt(pocetKand[i]);
-    if(pocetKandProFalesnyPrah > pocetMand) {
-      pocetKandProFalesnyPrah = pocetMand;
-    }
-    falesnyPrah[i] = parseInt(procenta[i]) * parseInt(pocetMand) / pocetKandProFalesnyPrah;
-  }
-
-  // snižování kvóra (bod 6)
-  for(i = 1; i < 5; i++) {
-
-    var pocetStranNadPrahem = 0;
-    var pocetKandidatuNadPrahem = 0;
-
-    for(j = 0; j < pocetStran; j++) {
-
-      if(falesnyPrah[j] > prah) {
-        pocetStranNadPrahem++;
-        pocetKandidatuNadPrahem = pocetKandidatuNadPrahem + pocetKand[j];
-      }
-
-    }
-
-    if(pocetStranNadPrahem < 2 || pocetKandidatuNadPrahem < pocetMand/2 || pocetKandidatuNadPrahem < 5) {
-      prah--;
-    }
-
-  }
-
-  // u jediné strany se kvórum nepočítá (bod 8)
-  if(pocetStran == 1) {
-    prah = 0;
-  }
-
-  // přepočet podle d'Hondta
-  for(i = 0; i < pocetStran; i++) {
-    prepocet[i] = new Array(pocetMand);
-  }
-
-  for(i = 0; i < pocetStran; i++) {
-    // j je dělitel
-    for(j = 1; j < pocetMand + 1; j++) {
-      if(falesnyPrah[i] > prah) {
-        prepocet[i][j-1] = procenta[i] * pocetKand[i] / j;
-      } else {
-        prepocet[i][j-1] = 0;
-      }
+  for(var i = 0; i < 9; i++) {
+    if(system == 'alternativni') {
+      vysledky[i+1] = document.getElementsByClassName(system)[i].value;
+      var button = document.getElementById("hlasovaniAlternativni");
+    } else if (system == 'approval') {
+      vysledky[i+1] = +(document.getElementsByClassName(system)[i].checked)
+      var button = document.getElementById("hlasovaniApproval");
+    } else if (system == 'janecek') {
+      vysledky[i+1] = +(document.getElementsByClassName('janecekplus')[i].checked);
+      vysledky[i+1] = +(document.getElementsByClassName('janecekminus')[i].checked);
+      var button = document.getElementById("hlasovaniJanecek");
     }
   }
 
-  // přidělení mandátů
-  var obsazeno = 0;
-  var max = 101;
+  button.innerHTML = text;
+  button.disabled = true;
+  button.style.opacity = 0.5;
 
-  while ( (obsazeno < pocetMand) & (max > 0) ) {
-
-    // nalezení nejvyšší hodnoty
-    var maxRow = prepocet.map(function(row) { return Math.max.apply(Math, row); });
-    max = Math.max.apply(null, maxRow);
-
-    // vymazání nejvyšší hodnoty a přidělení mandátu
-    for(i = 0; i < pocetStran; i++) {
-      for(j = 0; j < pocetMand; j++) {
-        if( (prepocet[i][j] == max) & (max > 0) ) {
-          prepocet[i][j] = 0;
-          max = 101;
-          if(mandaty[i] < pocetKand[i]) {
-            mandaty[i] = parseInt(mandaty[i]) + 1;
-            obsazeno++;
-          }
-        }
-      }
-    }
-  }
-
-  // mobilni hack pt. 2
-  if (!mobil) {
-    for(i = 0; i < pocetStran; i++) {
-      document.getElementsByClassName("mandaty")[i].textContent = mandaty[i];
-    }
-  } else {
-    for(i = 0; i < pocetStran; i++) {
-        document.getElementsByClassName("mandaty")[(i*2)+1].textContent = mandaty[i];
-    }
-  }
-
-  nakresliGraf(strany, procenta, mandaty);
-
-  document.getElementById("strany").style["padding-bottom"] = "30px";
-
-  document.getElementById("zpetStrany").scrollIntoView();
-
-  var html = '<button type="button" onclick="zpetNaKandidaty()">Zpět na kandidátky ↑</button>'
-
-  document.getElementById("zpet").innerHTML = html;
-
-}
-
-function poskladejTabulkuStran(seznamStran, idStran, idObce) {
-  var columns = poskladejHlavickuStran(seznamStran);
-
-  $('#tabulkaStran').append('<tbody>');
-
-  for (var i = 0; i < seznamStran.length; i++) {
-    var row$ = $('<tr/>');
-
-    for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-      var cellValue = seznamStran[i][columns[colIndex]];
-      var nazevStrany = '\'' + seznamStran[i]['Strana'] + '\'';
-
-      if (colIndex == 0) {
-        cellValue = '<div class="nazevStrany">' + cellValue + '</div>';
-      }
-
-      if (colIndex == 1) {
-        cellValue = '<div class="pocetKand">' + cellValue + '</div>';
-      }
-
-      if (colIndex == columns.length - 1) {
-        cellValue = '<div class="mandaty">' + cellValue + '</div>';
-      }
-
-      if (colIndex == columns.length - 2) {
-        cellValue = '<form onsubmit="return false"><div class="autocomplete percent"><input oninput="prepocitejProcenta()" class="vysledek" id="vysledekStrany" type="number" min="0" max="100" step="1" value="0"></div></form>'
-      }
-
-      if (cellValue == null) cellValue = "";
-      row$.append($('<td/>').html(cellValue));
-
-    }
-
-    $('#tabulkaStran').append(row$);
-
-  }
-
-};
-
-function poskladejHlavickuStran(seznamStran) {
-  var columnSet = [];
-
-  $('#tabulkaStran').append('<thead id="seznamStranHlavicka">');
-  var headerTr$ = $('<tr>');
-
-  for (var i = 0; i < seznamStran.length; i++) {
-    var rowHash = seznamStran[i];
-
-    for (var key in rowHash) {
-
-      if ($.inArray(key, columnSet) == -1) {
-        columnSet.push(key);
-        headerTr$.append($('<th/>').html(key));
-      }
-
-    }
-
-  }
-
-  $('#seznamStranHlavicka').append(headerTr$);
-
-  return columnSet;
-
-};
-
-function nakresliGraf(strany, procenta, mandaty) {
-
-  var mandatyCelkem = mandaty.reduce(function(a, b) { return a + b; }, 0);
-
-  for(i = 0; i < mandaty.length; i++) {
-    mandaty[i] = Math.round(100 * mandaty[i] / mandatyCelkem, 1);
-    procenta[i] = parseInt(procenta[i])
-  }
-
-  document.getElementById("vysledek").style["height"] = "600px";
-
-  var colors = ['#deebf7', '#2171b5']
-
-  var chart = Highcharts.chart('vysledek', {
-
-      chart: {
-          type: 'column'
-      },
-      title: {
-          text: 'Zisk hlasů vs. mandátů'
-      },
-      xAxis: {
-          categories: strany
-      },
-      yAxis: {
-          title: {
-              text: 'Zisk hlasů vs. mandátů'
-          },
-          labels: {
-              format: '{value} %'
-          }
-      },
-      tooltip: {
-          pointFormat: '{point.series.name}: <b>{point.y} %<br/>',
-          shared: true
-      },
-      exporting: {
-          enabled: false
-      },
-      credits: {
-          href: '',
-          text: ''
-      },
-      series: [{
-          name: 'Hlasy',
-          data: procenta,
-          color: colors[0]
-      }, {
-          name: 'Mandáty',
-          data: mandaty,
-          color: colors[1]
-      }]
-  });
-
-}
-
-function zpetNaKandidaty() {
-
-  document.getElementById("strany").scrollIntoView();
-
-}
-
-function sdilitko(obec, vitez) {
-  // vygenerování vyhodnocení
-  var sdileciURL = "https://www.facebook.com/sharer/sharer.php?u=https://www.irozhlas.cz/zpravy-nahled/kandidatky-tipovacka";
-  var sdileciURLtw = "https://twitter.com/intent/tweet?text=Dola%C4%8Fte%20formu%20na%20volby%3A%20vyzkou%C5%A1ejte%20si%20hlasov%C3%A1n%C3%AD%20ve%20sv%C3%A9%20obci%20nane%C4%8Disto.%20Tref%C3%ADte%20spr%C3%A1vn%C3%BD%20v%C3%BDsledek%3F&url=https%3A%2F%2Fwww.irozhlas.cz%2Fzpravy-nahled%2Fkandidatky-tipovacka";
-  var text = '<button id="sdilitko">Sdílet</button>';
-  text += '<button id="tweetitko">Tweetnout</button>';
-
-  $("#sdildiv").html(text);
-
-  $("#sdilitko").click(function() {
-    window.open(sdileciURL,'test','left=20,top=20,width=550,height=650,toolbar=0,resizable=0,menubar=0');
-  });
-
-  $("#tweetitko").click(function() {
-    window.open(sdileciURLtw,'test','left=20,top=20,width=550,height=250,toolbar=0,resizable=0,menubar=0');
-  });
-
-  // sdílítko - defaultní URL článku se dynamicky nahradí vygenerovanou
-  $.ajax({
-    url: encodeURI('https://nz6ctn80pc.execute-api.eu-central-1.amazonaws.com/prod?vals=' + JSON.stringify({
-      "obec": obec, "vitez": vitez
-    })),
-    type: "GET",
-    crossDomain: !0,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    dataType: "json",
-    success: function (response) {
-      sdileciURL = "https://www.facebook.com/sharer/sharer.php?u=https://data.irozhlas.cz/kandidatky-tipovacka/share/" + response + ".html";
-      sdileciURLtw = 'https://twitter.com/intent/tweet?url=https://data.irozhlas.cz/kandidatky-tipovacka/share/' + response + '.html&text=' + encodeURI('V obci ' + obec + ' podle mě zvítězí ' + vitez + '! Zkuste si také tipnout, jak dopadnou komunální volby ve Vaší obci.');
-      $("#sdilitko").click(function() {
-        window.open(sdileciURL,'test','left=20,top=20,width=550,height=650,toolbar=0,resizable=0,menubar=0');
-      });
-      $("tweetitko").click(function() {
-        window.open(sdileciURLtw,'test','left=20,top=20,width=550,height=250,toolbar=0,resizable=0,menubar=0');
-      })
-    }
-  });
+  return vysledky;
 }
